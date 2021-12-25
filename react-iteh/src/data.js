769d73c018622a5cec208f251102f37b252d4d88
bociwess0 <dimitrijeveljkovic2@gmail.com>
2021-12-25 19:02:15 +0100

    Fix styles

--- react-iteh/src/data.js
+++ react-iteh/src/data.js
@@ -19,28 +19,28 @@ const data = {
             name: 'Lenovo',
             model: 'Ideapad 3',
             price: 800,
-            image: 'https://i.expansys.net/img/b/363185/apple-macbook-pro-14-inch.jpg'
+            image: 'https://www.tehnomedia.rs/products/68359_huge_0_cached.jpg'
         },
         {
             id: 4,
             name: 'Lenovo',
             model: 'Ideapad 5',
             price: 1000,
-            image: 'https://i.expansys.net/img/b/363185/apple-macbook-pro-14-inch.jpg'
+            image: 'https://www.notebookcheck.net/uploads/tx_nbc2/LenovoIdeaPad5-15ALC05__1__01.jpg'
         },
         {
             id: 5,
             name: 'HP',
-            model: 'H6752-888',
+            model: 'Pavilion',
             price: 600,
-            image: 'https://i.expansys.net/img/b/363185/apple-macbook-pro-14-inch.jpg'
+            image: 'http://www.ctshop.rs//product/img/laptopovi/hp-pavilion-15-cw1024nm6ph46ealaptop-15-6fhd-amd-ryzen-7-3700u-8gb-1tb-128gb-ssd-radeon-vega-10-srebrni-3-cell/normal/71257011_7891991574.jpg'
         },
         {
             id: 6,
             name: 'Asus',
-            model: 'Gaming',
+            model: 'ZenBook',
             price: 1450,
-            image: 'https://i.expansys.net/img/b/363185/apple-macbook-pro-14-inch.jpg'
+            image: 'http://www.ctshop.rs//img/products/2020-11-03/asus-zenbook-pro-duo-ux581lv-h2002r-laptop-intel-hexa-core-i7-10750h-15-6-quot-uhd-16gb-1tb-ssd-geforce-rtx2060-win10-pro-plavi_AJJjE_3.jpg'
         },
     ]
 }
